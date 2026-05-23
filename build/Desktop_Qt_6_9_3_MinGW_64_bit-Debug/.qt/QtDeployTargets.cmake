ebfdbfdda3f515d3fd3d31ac7a4d42106d03bab6
vladdeb <vlabislavshimko2006@gmail.com>
2026-05-23 16:59:35 +0300

    clear

--- build/Desktop_Qt_6_9_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_WhiteBoardServer_FILE C:/Users/Lenovo/Desktop/BSUIR/!sem4/Course/WhiteBoardServer/build/Desktop_Qt_6_9_3_MinGW_64_bit-Debug/WhiteBoardServer.exe)
-set(__QT_DEPLOY_TARGET_WhiteBoardServer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_WhiteBoardServer_RUNTIME_DLLS C:/Qt/6.9.3/mingw_64/bin/Qt6Network.dll;C:/Qt/6.9.3/mingw_64/bin/Qt6Core.dll)
